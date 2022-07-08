9b1b1c2ee8751219496d9a5bb6a6ecf877fd15be
stellar-creator <stelofme@gmail.com>
2022-07-08 15:04:23 +0500

    Change GUI to Qt5

--- uCrewProjectUploader/plugins/__init__.py
+++ uCrewProjectUploader/plugins/__init__.py
@@ -1,5 +1,6 @@
 import pcbnew, os, sys
 
 from .ucp_uploader import uCrewProjectsUploader
+from .ui import Ui_MainWindow
 
 uCrewProjectsUploader().register()
--- uCrewProjectUploader/plugins/ucp_uploader.py
+++ uCrewProjectUploader/plugins/ucp_uploader.py
@@ -1,4 +1,13 @@
 import pcbnew, os, sys, json
+from PyQt5 import QtWidgets
+from .ui import Ui_MainWindow
+
+class GUI(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
 
 class uCrewProjectsUploader(pcbnew.ActionPlugin):
     def defaults(self):
@@ -11,6 +20,10 @@ class uCrewProjectsUploader(pcbnew.ActionPlugin):
     def Run(self):
         # The entry function of the plugin that is executed on user action
         print("Hello World")
+        self.showAthorization()
 
     def showAthorization(self):
-        pass
+        app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+        window = GUI()  # Создаём объект класса ExampleApp
+        window.show()  # Показываем окно
+        app.exec_()  # и запускаем приложение
